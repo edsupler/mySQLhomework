821ca4397937a844abc0a7832aaa450c2a5b4243
Eduard Supler <94659251+edsupler@users.noreply.github.com>
2022-02-07 14:06:54 -0500

    Add files via upload

--- Week2.sql
+++ Week2.sql
@@ -5,3 +5,8 @@ GROUP BY title;
 SELECT t.title as "Title", avg(s.salary) as "Average Salary" from salaries s
 INNER JOIN titles t ON s.emp_no = t.emp_no
 GROUP BY title;
+SELECT ds.dept_name AS 'Department Name', FORMAT (sum(s.salary),2) AS "Total Spent" FROM dept_emp AS d
+INNER JOIN salaries AS s ON d.emp_no = s.emp_no AND s.from_date >= '1990-01-01' AND s.from_date <= '1992-01-01'
+INNER JOIN departments AS ds ON ds.dept_no = d.dept_no
+WHERE ds.dept_name = 'Marketing'  
+GROUP BY ds.dept_name;
